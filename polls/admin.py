cff9adb3b313cdb8789a369cce96ce80d446f773
Kelly Yeh <kellyyeh1125@gmail.com>
2020-03-15 08:23:42 -0700

    updated login

--- polls/admin.py
+++ polls/admin.py
@@ -1,7 +1,5 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User, Student
+from .models import User
 
-#admin.site.register(User)
-admin.site.register(Student)
 
